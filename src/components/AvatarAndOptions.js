20ae48af58a0d091da2c0ab0ebc7827e016e9dd3
DouyaQki <douyaqki@gmail.com>
2023-01-03 18:57:28 -0300

    mouseover/leave added on the follow button.

--- src/components/AvatarAndOptions.js
+++ src/components/AvatarAndOptions.js
@@ -9,8 +9,10 @@ const AvatarAndOptions = () => {
     const CONSIDER_THE_FOLLOWING_CLASS = 'consider-the-following'
     const BTN_FOLLOW = followRef.current
 
-    const userDontFollow = !BTN_FOLLOW.className.includes(CONSIDER_THE_FOLLOWING_CLASS)
-    if(userDontFollow) {
+    const userDontFollow = !BTN_FOLLOW.className.includes(
+      CONSIDER_THE_FOLLOWING_CLASS
+    )
+    if (userDontFollow) {
       BTN_FOLLOW.classList.add(CONSIDER_THE_FOLLOWING_CLASS)
       BTN_FOLLOW.textContent = 'Following'
       return
@@ -20,16 +22,63 @@ const AvatarAndOptions = () => {
     BTN_FOLLOW.textContent = 'Follow'
   }
 
+  // handleMouseOver and handleMouseLeave are for the Follow button.
+  const handleMouseOver = (e) => {
+    // console.log(e.target.textContent = 'PIPE')
+    const followBtnClass = [...e.target.classList]
+
+    const BTN_SHOW_UNFOLLOW = e.target
+
+    const userIsFollowing = followBtnClass.includes('consider-the-following')
+
+    if (userIsFollowing) {
+      BTN_SHOW_UNFOLLOW.textContent = 'Unfollow'
+      BTN_SHOW_UNFOLLOW.style.color = '#cc3333'
+      BTN_SHOW_UNFOLLOW.style.border = 'solid 1px #cc3333'
+      return
+    }
+  }
+
+  const handleMouseLeave = (e) => {
+    // console.log(e.target.textContent = 'PIPE')
+    const followBtnClass = [...e.target.classList]
+    const BTN_SHOW_UNFOLLOW = e.target
+
+    const userIsFollowing = followBtnClass.includes('consider-the-following')
+
+    if (userIsFollowing) {
+      BTN_SHOW_UNFOLLOW.textContent = 'Follow'
+      BTN_SHOW_UNFOLLOW.style.color = ''
+      BTN_SHOW_UNFOLLOW.style.border = ''
+      return
+    }
+
+    BTN_SHOW_UNFOLLOW.style.color = ''
+    BTN_SHOW_UNFOLLOW.style.border = ''
+  }
+
   return (
     <section className='avatar-notification-options'>
-        <img src={account_avatar} alt='account avatar' className='account_avatar' />
+      <img
+        src={account_avatar}
+        alt='account avatar'
+        className='account_avatar'
+      />
 
       <section className='options-box'>
         <div className='account-btn'>
           <img src={options_icon} alt='option icon' className='option' />
         </div>
 
-        <button className='follow-btn' ref={followRef} onClick={handleClick}>Follow</button>
+        <button
+          className='follow-btn'
+          ref={followRef}
+          onClick={handleClick}
+          onMouseOver={handleMouseOver}
+          onMouseLeave={handleMouseLeave}
+        >
+          Follow
+        </button>
       </section>
     </section>
   )
